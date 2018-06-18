27498e9c5eeae5179f50062348b799e43ab329b8
Abiaina <abinnet.a@gmail.com>
2018-06-18 16:12:24 -0700

    added search movie component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -44,7 +44,7 @@ class SearchForm extends Component {
       return (
         <form onSubmit={this.onFormSubmit}>
           <div key="movieName">
-            <label htmlFor="movieName">Movie Name: </label>
+            <label htmlFor="movieName">Movie: </label>
             <input
               id="movieName"
               name="movieName"
